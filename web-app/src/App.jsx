8c4625ddcae9dfe103eecf1c148bd31b952966df
Renen Maharaj <someshrenen@gmail.com>
2025-10-01 02:07:46 +0200

    added the frontend framework with vite and tailwind

---
+++ web-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400">Tailwind v3 is working 🚀</h1>
+    </div>
+  );
+}
+
+
+
+
